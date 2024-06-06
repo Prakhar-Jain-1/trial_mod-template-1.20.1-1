8e94d791936780ab19626cb69102848db071b3a3
Prakhar-Jain-1 <prakharjain582@gmail.com>
2024-06-06 14:54:14 +0530

    first commit

---
+++ src/main/java/net/prakhar/trial_mod/Trial_ModClient.java
@@ -0,0 +1,10 @@
+package net.prakhar.trial_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Trial_ModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/prakhar/trial_mod/Trial_ModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.prakhar.trial_mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Trial_ModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
